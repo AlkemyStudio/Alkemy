a07163f9e226075a786800a6a0a1a9ad7405f344
Alphamplyer <even@pouyadou.fr>
2023-04-08 11:22:32 +0200

    Fix the bomb shader to take account of voxel colors

--- Assets/Scripts/Bomb/ClassicBombController.cs
+++ Assets/Scripts/Bomb/ClassicBombController.cs
@@ -1,10 +1,25 @@
-using System.Collections;
 using UnityEngine;
 
 namespace Bomb
 {
     public class ClassicBombController : BaseBombController
     {
-        
+        [SerializeField] private AnimationCurve flickeringCurve;
+        [SerializeField] private float baseFlickeringSpeed;
+        [SerializeField] private float finalFlickeringSpeed;
+
+        private static readonly int FlickerSpeed = Shader.PropertyToID("_FlickerSpeed");
+
+        private void Update()
+        {
+            float bombLifeTime = Mathf.Min(Time.time - spawnTime, BombData.FuseTime) / BombData.FuseTime;
+            float curveValue = flickeringCurve.Evaluate(bombLifeTime);
+            float flickerValue = Mathf.Lerp(baseFlickeringSpeed, finalFlickeringSpeed, curveValue);
+
+            meshRenderer.sharedMaterial.SetFloat(
+                FlickerSpeed,
+                flickerValue
+            );
+        }
     }
 }
